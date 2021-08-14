c3af3864605f3183d3f3bb55f28126bdc1fd1e78
Raymond <rayjxclusive@gmail.com>
2021-08-14 15:01:15 +0300

    added new url listings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 import environ
 # Initialise environment variables
 env = environ.Env()
@@ -25,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-z+14m@u8wsaht#zvq**cb^8pew=q5e$l^gi+&1zcz%@mil9c#o'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
