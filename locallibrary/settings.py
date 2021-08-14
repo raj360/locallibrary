baf3e8ef79a9ca169e6092cb526ec55b91f94a05
Raymond <rayjxclusive@gmail.com>
2021-08-14 14:17:28 +0300

    update secrete

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z+14m@u8wsaht#zvq**cb^8pew=q5e$l^gi+&1zcz%@mil9c#o'
-
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
