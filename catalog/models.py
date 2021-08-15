384dc98d49c60be643e56935d7423b791a68f063
Raymond <rayjxclusive@gmail.com>
2021-08-15 10:53:51 +0300

    update created the Book Model

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -10,3 +11,31 @@ class Genre(models.Model):
     def __str__(self):
         """String for representing the Model object"""
         return self.name
+
+
+class Book(models.Model):
+    """Model representing a book (But not a specific copy of a book)"""
+    title = models.CharField(max_length=200)
+
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author as a string rather than object because it hasn't been declared yet in the file
+    # author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the Book")
+    isbn = models.CharField('ISBN',
+                            max_length=13,
+                            unique=True,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a> '
+                            )
+
+    genre = models.ManyToManyField(Genre, help_text="Select A genre for this book")
+
+    def __str__(self):
+        """String for Representing the model Object """
+        return self.title
+
+    def get_absolute_url(self):
+        """Returning the url to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+
