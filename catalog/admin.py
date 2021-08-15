83e4d3049cae292bb9c0b758c18ec18b280b19f1
Raymond <rayjxclusive@gmail.com>
2021-08-15 20:59:21 +0300

    updated added a list filter to the BookAdminInstance view

--- catalog/admin.py
+++ catalog/admin.py
@@ -15,7 +15,7 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
 
 
 # Register your models here.
