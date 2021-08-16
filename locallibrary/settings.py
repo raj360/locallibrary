833a32012b8ae130fbbc5a167f202cf6e3bae7a2
Raymond <rayjxclusive@gmail.com>
2021-08-17 00:37:28 +0300

    update SECRET_KEY for locallibrary app

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -22,12 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-z+14m@u8wsaht#zvq**cb^8pew=q5e$l^gi+&1zcz%@mil9c#o'
 # SECRET_KEY = os.environ.get('SECRET_KEY')
-print(os.getenv("DB_NAME", "just_a_test"))
-print(os.getenv('SECRET_KEY'))
 
-print(os.getenv('KEY'))
-
-print(os)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
